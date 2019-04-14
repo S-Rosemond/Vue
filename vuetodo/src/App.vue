95dec51a1526168eb143e4a7e19afe205997d061
Samuel <js.rosemond@gmail.com>
2019-04-14 18:13:16 -0400

    View basics, tomorrow request/router

---
+++ vuetodo/src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="app">
+    <Header/>
+    <AddTodo v-on:add-todo="addTodo"/>
+    <Todos v-bind:todos="todos" v-on:del-todo="deletTodo"/>
+  </div>
+</template>
+
+<script>
+import Todos from "./components/Todos.vue";
+import Header from "./components/layout/Header.vue";
+import AddTodo from "./components/AddTodo.vue";
+
+export default {
+  name: "app",
+  components: {
+    Todos,
+    Header,
+    AddTodo
+  },
+  data() {
+    return {
+      todos: [
+        { id: 1, title: "test1", completed: false },
+        { id: 2, title: "test2", completed: false },
+        { id: 3, title: "test3", completed: false },
+        { id: 4, title: "type Array test", completed: true }
+      ]
+    };
+  },
+  methods: {
+    deletTodo(id) {
+      this.todos = this.todos.filter(el => el.id !== id);
+    },
+    addTodo(newTodo) {
+      this.todos = [...this.todos, newTodo];
+    }
+  }
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 1.4;
+}
+
+.btn {
+  display: inline-block;
+  border: none;
+  background: #555;
+  color: #fff;
+  padding: 12px 20px;
+  cursor: pointer;
+}
+.btn:hover {
+  background: #666;
+}
+</style>
